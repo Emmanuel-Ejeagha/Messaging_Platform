e3211fca59051fd91d1f46b7d4ad63d5d352c72f
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-13 17:07:14 +0100

    Add Aggregate interface

---
+++ src/MessagingPlatform.Domain/Common/IAggregateRoot.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace MessagingPlatform.Domain.Common;
+
+public interface IAggregateRoot
+{
+    // Marker interface for aggregate roots
+    // Useful for generic constraints in repositories
+}
