0af33cddc45307a0f380c92e693c571633b0cd9a
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-13 17:07:45 +0100

    Add ValueObject

---
+++ src/MessagingPlatform.Domain/Common/ValueObject.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace MessagingPlatform.Domain.Common;
+
+public abstract class ValueObject
+{
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+            return false;
+
+        var other = (ValueObject)obj;
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return !Equals(left, right);
+    }
+
+    protected static bool EqualsOperator(ValueObject left, ValueObject right)
+    {
+        if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+            return false;
+
+        return ReferenceEquals(left, null) || left.Equals(right);
+    }
+    
+    protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+    {
+        return !EqualsOperator(left, right);
+    }
+    
+
+}
