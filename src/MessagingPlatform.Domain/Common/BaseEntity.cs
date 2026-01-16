ce542d90ad47a47729d20f84c42171253613fa9c
Emmanuel-Ejeagha <emma.ejeagha@gmail.com>
2026-01-16 12:35:36 +0100

    Add Base Entity

--- src/MessagingPlatform.Domain/Common/BaseEntity.cs
+++ src/MessagingPlatform.Domain/Common/BaseEntity.cs
@@ -5,13 +5,13 @@ namespace MessagingPlatform.Domain.Common;
 public abstract class BaseEntity
 {
     public Guid Id { get; protected set; } = Guid.NewGuid();
-    public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; protected set; }
 
     private readonly List<DomainEvent> _domainEvent = new();
     public IReadOnlyCollection<DomainEvent> DomainEvents => _domainEvent.AsReadOnly();
     protected void AddDomainEvent(DomainEvent domainEvent) => _domainEvent.Add(domainEvent);
     public void ClearDomainEvents() => _domainEvent.Clear();
-    protected void UpdateTimestamp() => UpdatedAt = DateTime.Now;
+    public void UpdateTimestamp() => UpdatedAt = DateTime.Now;
 }
 
